c41d99364010fcdd1e582d5a568d8fae6144fec1
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-16 21:56:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,9 +10,10 @@ def find_min_in_nested_arrays(src)
         end
         element_index += 1
       end
+    daily_low_temp << src[row_index][element_index]
+    row_index += 1 
   end
-  daily_low_temp << src[row_index][element_index]
-  row_index += 1 
+  
 end
 
 # src will be an array of arrays of integers
